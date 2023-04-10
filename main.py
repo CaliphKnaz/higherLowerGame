527981379b60bc677d768ec4099b52c0284ae5b9
Tomi <48963553+CaliphKnaz@users.noreply.github.com>
2023-04-10 16:13:46 +0100

    added comments

--- main.py
+++ main.py
@@ -3,29 +3,32 @@ import random
 
 
 def randomAccount():
+    """Retireve random account data"""
     account = random.randint(1, 49)
     return data[account]
 
 
 def compareAccounts(account, account2):
+    """Compare two retreived accounts from data"""
     print(
         f"{account['name']}, a {account['description']} , from {account['country']}")
     print(
         f"{account2['name']}, a {account2['description']} , from {account2['country']}")
 
+    # compare the follower account from the retrieved account data and return the correct one
     if account["follower_count"] > account2["follower_count"]:
-        # print(f" {account['name']} has a larger follower account")
-        print("A")
+        # print("A")
         return account
     else:
-        # print(f" {account2['name']} has a larger follower account")
-        print("B")
+        # print("B")
         return account2
 
 
 def userChoice(account, account2):
+    """Check if the user answerd correctly based on the two account displayed"""
     continue_game = True
     while continue_game:
+        # store the correct account in a variable 'result'
         result = compareAccounts(account, account2)
         if result == account2:
             answer = "B"
@@ -36,16 +39,19 @@ def userChoice(account, account2):
             "Which account has a higher following?. \n 'A' or 'B'").upper()
 
         if choice == answer:
+            # add to score counter
             global score_counter
             score_counter += 1
 
             print(score_counter)
             print("Correct")
+            # keep the correct answer and compare it with a random one
             account2 = randomAccount()
             userChoice(account=result, account2=account2)
 
             return True
         else:
+            # exit the game when answered incorrectly
             print(f"That was incorrect \n Final score was {score_counter} ")
             continue_game = False
             return False
@@ -54,5 +60,5 @@ def userChoice(account, account2):
 score_counter = int()
 account = randomAccount()
 account2 = randomAccount()
-# continue_game = True
+# run the coder
 verdict = userChoice(account=account, account2=account2)
